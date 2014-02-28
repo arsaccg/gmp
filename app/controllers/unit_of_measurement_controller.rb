52606a8e2eb926cf79760634e1fce7d8afde3ff3
Eduardo Cabrera <dvex.cabrera@gmail.com>
2014-02-28 12:21:05 -0500

    Creando la funcionalidad y vista de creacion de unidad de medida

--- app/controllers/unit_of_measurement_controller.rb
+++ app/controllers/unit_of_measurement_controller.rb
@@ -1,10 +1,18 @@
 class UnitOfMeasurementController < ApplicationController
   def index
-    @unitOfMeasures = UnitOfMeasure.all
+    @unitOfMeasures = UnitOfMeasurement.all
     render layout: false
   end
 
   def create
+    @unitOfMeasure = UnitOfMeasurement.new(unit_of_measure_params)
+    if @unitOfMeasure.save
+      flash[:notice] = "Se ha creado correctamente la nueva unidad de medida."
+      redirect_to :action => 'index'
+    else
+      flash[:error] = "Ha ocurrido un problema. Porfavor, contactar con el administrador del sistema."
+      redirect_to :action => 'index'
+    end
   end
 
   def edit
@@ -17,8 +25,16 @@ class UnitOfMeasurementController < ApplicationController
   end
 
   def new
+    @unitOfMeasure = UnitOfMeasurement.new
+    render :layout => 'dashboard'
   end
 
   def destroy
   end
+
+  private
+    def unit_of_measure_params
+      params.required(:unit_of_measurement).permit(:name)
+    end
+
 end
