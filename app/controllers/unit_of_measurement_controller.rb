5f30a14e52bae8943e8a3a85d7e4fdba679c3a9f
giancarlo <giancarlo.paredes.prieto@gmail.com>
2014-02-28 10:19:55 -0500

    add views um

--- app/controllers/unit_of_measurement_controller.rb
+++ app/controllers/unit_of_measurement_controller.rb
@@ -1,24 +1,22 @@
 class UnitOfMeasurementController < ApplicationController
-  # GET /anhos
-  # GET /anhos.json
   def index
-    @UnitOfMeasurement = UnitOfMeasurement.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @@UnitOfMeasurement }
-    end
   end
 
-  # GET /anhos/1
-  # GET /anhos/1.json
+  def create
+  end
+
+  def edit
+  end
+
   def show
-    @UnitOfMeasurement = UnitOfMeasurement.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @UnitOfMeasurement }
-    end
   end
 
+  def update
+  end
+
+  def new
+  end
+
+  def destroy
+  end
 end
