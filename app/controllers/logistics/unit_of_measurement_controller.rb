516cb5ef2be93ce2e9425bcdc5d6050a24990ba9
joel ibaceta <jibaceta@incloudsgroup.com>
2014-02-28 11:29:54 -0500

    data

--- app/controllers/logistics/unit_of_measurement_controller.rb
+++ app/controllers/logistics/unit_of_measurement_controller.rb
@@ -5,20 +5,41 @@ class UnitOfMeasurementController < ApplicationController
   end
 
   def create
+    unitOfMeasure = UnitOfMeasure.new(unit_of_measure_parameters)
+    if unitOfMeasure.save?
+      redirec_to :index
+    else
+      render :new
+    end
+
   end
 
   def edit
+    unitOfMeasure = unitOfMeasure.find(params[:id])
+    render :edit
   end
 
   def show
+    unitOfMeasure = unitOfMeasure.find(params[:id])
+    render :show
   end
 
   def update
+    unitOfMeasure = unitOfMeasure.find(params[:id])
+    
   end
 
   def new
+    unitOfMeasure = UnitOfMeasure.new
+    render :new
   end
 
   def destroy
   end
+
+  private
+  def unit_of_measure_parameters
+    params.require(:unit_of_measure).permit(:name)
+  end
+
 end
