774f551e1cd2d675ee48cf687b3e734e3d3e2963
Moises <elnaufrago2009@gmail.com>
2014-03-01 12:59:29 -0500

    database xd

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140301055049) do
+ActiveRecord::Schema.define(version: 20140301175259) do
 
   create_table "articles", force: true do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20140301055049) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "code"
   end
 
   create_table "delivery_order_details", force: true do |t|
