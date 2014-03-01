1a8d512377ae1552b4234b3b4652266b9529f0a3
Eduardo Cabrera <dvex.cabrera@gmail.com>
2014-03-01 11:25:15 -0500

    Añadiendo jquery form

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,4 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "28x25>" }, :default_url => "/assets/images/avatars/male.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
-  
-
 end 
