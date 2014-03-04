f8c0cd15f42b9874ff357a111a0dc8aa9fde477a
Eduardo Cabrera <dvex.cabrera@gmail.com>
2014-03-04 11:53:50 -0500

    Resolviendo mas conflictos

--- app/models/delivery_order.rb
+++ app/models/delivery_order.rb
@@ -1,11 +1,7 @@
 class DeliveryOrder < ActiveRecord::Base
 	 
 	has_many :state_per_order_details
-<<<<<<< HEAD
 	has_many :delivery_order_details
-
-=======
->>>>>>> 011f18f2cde719433daf750feba7edcf41f7938a
 	belongs_to :user
 	belongs_to :sector
 	belongs_to :phase
