4977d94afce0b0c259c33b825439377ef5025c47
Eduardo Cabrera <dvex.cabrera@gmail.com>
2014-04-11 10:37:39 -0500

    Terminando componentes

--- app/models/componet.rb
+++
@@ -1,2 +0,0 @@
-class Componet < ActiveRecord::Base
-end
--- db/migrate/20140410162754_create_componets.rb
+++
@@ -1,9 +0,0 @@
-class CreateComponets < ActiveRecord::Migration
-  def change
-    create_table :componets do |t|
-      t.string :type
-
-      t.timestamps
-    end
-  end
-end
