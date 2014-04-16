0f668390a11cbf411ed3d8a71aaa6a52c4a8f96f
Eduardo Cabrera <dvex.cabrera@gmail.com>
2014-04-16 18:45:10 -0500

    Cambios de Actualizacion de montos, IPC, archivos trolls, etc

--- app/controllers/biddings/works_controller.rb
+++ app/controllers/biddings/works_controller.rb
@@ -8,6 +8,10 @@ class Biddings::WorksController < ApplicationController
     @work = Work.find(params[:id])
     @components = @work.components
     @work_partners = @work.work_partners
+    @financial_variables = Array.new
+    FinancialVariable.where("name LIKE 'IPC'").each do |fvar|
+      @financial_variables = fvar
+    end
     render layout: false
   end
 
@@ -17,6 +21,10 @@ class Biddings::WorksController < ApplicationController
     @moneys = Money.all
     @entities = Array.new
     @contractors = Array.new
+    @financial_variables = Array.new
+    FinancialVariable.where("name LIKE 'IPC'").each do |fvar|
+      @financial_variables = fvar
+    end
     TypeEntity.where("name LIKE 'Clientes'").each do |tent|
       @entities << tent.entities
     end
@@ -76,6 +84,6 @@ class Biddings::WorksController < ApplicationController
 
   private
   def work_params
-    params.require(:work).permit(:exchange_of_rate, :money_id, {:work_partner_ids => []}, :budget, :arbitration, :start_date_of_inquiry, :end_date_of_inquiry, :integrated_bases, :procurement_system, :purpose_of_contract, :date_signature_of_contract, :start_date_of_work, :real_end_date_of_work, :date_of_receipt_of_work, :settlement_date, :specialty, :name, :entity_id, :participation_of_arsac, :contractor_id, :amount_of_contract, :amount_of_settlement, :ipc_settlement, :testimony_of_consortium, :contract, :reception_certificate, :settlement_of_work, {:component_ids => []})
+    params.require(:work).permit(:number_of_settlement, :exchange_of_rate, :money_id, {:work_partner_ids => []}, :budget, :arbitration, :start_date_of_inquiry, :end_date_of_inquiry, :integrated_bases, :procurement_system, :purpose_of_contract, :date_signature_of_contract, :start_date_of_work, :real_end_date_of_work, :date_of_receipt_of_work, :settlement_date, :specialty, :name, :entity_id, :participation_of_arsac, :contractor_id, :amount_of_contract, :amount_of_settlement, :ipc_settlement, :testimony_of_consortium, :contract, :reception_certificate, :settlement_of_work, {:component_ids => []})
   end
 end
