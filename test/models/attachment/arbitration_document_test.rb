f4d0a6e5822ba7efe475cc51ac19442432722608
Eduardo Cabrera <dvex.cabrera@gmail.com>
2014-04-24 18:13:01 -0500

    Eliminando migrates y modelos con prefijos

--- test/models/attachment/arbitration_document_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Attachment::ArbitrationDocumentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/attachment/integrated_bases_document_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Attachment::IntegratedBasesDocumentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
