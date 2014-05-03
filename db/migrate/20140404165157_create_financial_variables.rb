f9e5eb95a5cfcb8b8958e7d8171fe23d001a06e6
Eduardo Cabrera <dvex_92@hotmail.com>
2014-05-02 23:27:47 -0500

    Migrate faltante de financial variables

---
+++ db/migrate/20140404165157_create_financial_variables.rb
@@ -0,0 +1,10 @@
+class CreateFinancialVariables < ActiveRecord::Migration
+  def change
+    create_table :financial_variables do |t|
+      t.string :name
+      t.float :value
+
+      t.timestamps
+    end
+  end
+end
