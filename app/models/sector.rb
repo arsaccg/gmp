ef97cfed92c485c7dd839fe5240b9969e1083db2
Eduardo Cabrera <dvex.cabrera@gmail.com>
2014-05-06 17:00:00 -0500

    Agregando lo de subsectores

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -6,4 +6,8 @@ class Sector < ActiveRecord::Base
 	include ActiveModel::Validations
 	#validates :code, :uniqueness => { :scope => :name, :message => "El código debe ser único" }
 	validates :code, :uniqueness => { :message => "El código debe ser único."}
+	
+	def self.getSubSectors(sector_code)
+	  return Sector.where("code LIKE '#{sector_code}__'")
+	end
 end
