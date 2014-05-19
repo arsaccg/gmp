ff6e80807522cadae74d022b80592a195ebcd00b
Eduardo Cabrera <dvex.cabrera@gmail.com>
2014-05-19 10:46:06 -0500

    Terminando Parte diario de Personal 100%

--- sigo/app/models/part_person.rb
+++ sigo/app/models/part_person.rb
@@ -1,5 +1,6 @@
 class PartPerson < ActiveRecord::Base
-	has_many :part_person_details
-	belongs_to :working_group
-	accepts_nested_attributes_for :part_person_details, :allow_destroy => true
+  has_many :part_person_details
+  belongs_to :working_group
+  accepts_nested_attributes_for :part_person_details, :allow_destroy => true
+
 end
--- sigo/app/models/worker.rb
+++ sigo/app/models/worker.rb
@@ -4,4 +4,12 @@ class Worker < ActiveRecord::Base
 	has_many :worker_details
 
 	accepts_nested_attributes_for :worker_details, :allow_destroy => true
+
+	def self.find_name_front_chief(front_chief_id)
+	  return Worker.find(front_chief_id).first_name + ' ' + Worker.find(front_chief_id).second_name + ' ' + Worker.find(front_chief_id).paternal_surname + ' ' + Worker.find(front_chief_id).maternal_surname
+	end
+
+	def self.find_name_master_builder(master_builder_id)
+	  return Worker.find(master_builder_id).first_name + ' ' + Worker.find(master_builder_id).second_name + ' ' + Worker.find(master_builder_id).paternal_surname + ' ' + Worker.find(master_builder_id).maternal_surname
+	end
 end
