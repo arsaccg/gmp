1f6033cc6346f28795f3e51c205c0ff91cb57ab7
giancarlo <giancarlo.paredes.prieto@gmail.com>
2014-05-20 12:13:43 -0400

    update reports inventario SP

--- sigo/app/models/stock_input_detail.rb
+++ sigo/app/models/stock_input_detail.rb
@@ -10,21 +10,36 @@ class StockInputDetail < ActiveRecord::Base
     self.status = "A"
   end
 
-  def self.get_kardex_detail(user, date_type, from_date, to_date)
+  def self.get_kardex_summary(updatefilter, user, report_type, date_type, from_date, to_date, cost_centers, warehouses, suppliers, years, periods, formats, articles, moneys)
     case date_type
       when "1" # Periodo / Calendar
+        ActiveRecord::Base.connection.execute("CALL REP_INV_KARDEX_CALENDAR(#{updatefilter}, #{report_type}, 4, #{user}, '#{from_date}', '#{to_date}', '#{cost_centers}', '#{warehouses}', '#{suppliers}', '#{years}', '#{periods}', '#{formats}', '#{articles}', '#{moneys}');")
+      when "2" # TimeLine by Cost Center
+        ActiveRecord::Base.connection.execute("CALL REP_INV_KARDEX_COST_CENTER(#{updatefilter}, #{report_type}, 4, #{user}, '#{from_date}', '#{to_date}', '#{cost_centers}', '#{warehouses}', '#{suppliers}', '#{years}', '#{periods}', '#{formats}', '#{articles}', '#{moneys}');")
+    end
+  end
+
+  def self.get_kardex_daily(updatefilter, user, report_type, date_type, from_date, to_date, cost_centers, warehouses, suppliers, years, periods, formats, articles, moneys)
+    case date_type
+      when "1" # Periodo / Calendar
+        ActiveRecord::Base.connection.execute("CALL REP_INV_KARDEX_CALENDAR(#{updatefilter}, #{report_type}, 3, #{user}, '#{from_date}', '#{to_date}', '#{cost_centers}', '#{warehouses}', '#{suppliers}', '#{years}', '#{periods}', '#{formats}', '#{articles}', '#{moneys}');")
+      when "2" # TimeLine by Cost Center
+        ActiveRecord::Base.connection.execute("CALL REP_INV_KARDEX_COST_CENTER(#{updatefilter}, #{report_type}, 3, #{user}, '#{from_date}', '#{to_date}', '#{cost_centers}', '#{warehouses}', '#{suppliers}', '#{years}', '#{periods}', '#{formats}', '#{articles}', '#{moneys}');")
+      when "3" # Periodo / Calendar
         joins{stock_input.warehouse}
+        .joins{stock_input.rep_inv_warehouse}
+        .joins{stock_input.rep_inv_supplier}
+        .joins{stock_input.rep_inv_year}
+        .joins{stock_input.rep_inv_period}
+        .joins{stock_input.rep_inv_format}
         .joins{purchase_order_detail.outer.delivery_order_detail.outer.article.outer.unit_of_measurement.outer}
         .joins{article.outer.unit_of_measurement.outer}
         .joins{article.outer.rep_inv_article.outer}
         .joins{purchase_order_detail.outer.delivery_order_detail.outer.rep_inv_article.outer}
-        .joins{stock_input.rep_inv_warehouse}
-        .joins{stock_input.rep_inv_supplier}
-        .joins{stock_input.rep_inv_period}
-        .joins{stock_input.rep_inv_format}
         .joins{purchase_order_detail.outer.purchase_order.outer.rep_inv_money.outer}
         .where{(rep_inv_warehouses.user.eq "#{user}") &
                (rep_inv_suppliers.user.eq "#{user}") &
+               (rep_inv_years.user.eq "#{user}") &
                (rep_inv_periods.user.eq "#{user}") &
                (rep_inv_formats.user.eq "#{user}") &
                (
@@ -44,21 +59,22 @@ class StockInputDetail < ActiveRecord::Base
         .select{('coalesce(unit_of_measurements_articles.symbol,unit_of_measurements.symbol) AS article_symbol')}
         .select('stock_input_details.amount')
         .reorder('period, issue_date, article_name')
-      when "2" # TimeLine by Cost Center
+      when "4" # TimeLine by Cost Center
         joins{stock_input.warehouse}
-        .joins{stock_input.link_period}
+        .joins{stock_input.cost_center_timeline.rep_inv_year}
+        .joins{stock_input.cost_center_timeline.rep_inv_period}
+        .joins{stock_input.rep_inv_warehouse}
+        .joins{stock_input.rep_inv_supplier}
+        .joins{stock_input.rep_inv_format}
         .joins{purchase_order_detail.outer.delivery_order_detail.outer.article.outer.unit_of_measurement.outer}
         .joins{article.outer.unit_of_measurement.outer}
         .joins{article.outer.rep_inv_article.outer}
         .joins{purchase_order_detail.outer.delivery_order_detail.outer.rep_inv_article.outer}
-        .joins{stock_input.rep_inv_warehouse}
-        .joins{stock_input.rep_inv_supplier}
-        .joins{stock_input.rep_inv_period}
-        .joins{stock_input.rep_inv_format}
         .joins{purchase_order_detail.outer.purchase_order.outer.rep_inv_money.outer}
-        .where{(link_periods.cost_center_id.eq warehouses.cost_center_id) &
+        .where{(cost_center_timelines.cost_center_id.eq warehouses.cost_center_id) &
                (rep_inv_warehouses.user.eq "#{user}") &
                (rep_inv_suppliers.user.eq "#{user}") &
+               (rep_inv_years.user.eq "#{user}") &
                (rep_inv_periods.user.eq "#{user}") &
                (rep_inv_formats.user.eq "#{user}") &
                (
@@ -70,32 +86,38 @@ class StockInputDetail < ActiveRecord::Base
         .select{stock_inputs.input}
         .select{stock_inputs.warehouse_id}
         .select{warehouses.name.as(warehouse_name)}
-        .select{link_periods.month.as(period)}
+        .select{cost_center_timelines.period}
         .select{stock_inputs.issue_date}
         .select{coalesce(article_id, delivery_order_details.article_id).as(article_id)}
         .select{coalesce(articles_stock_input_details.code,articles.code).as(article_code)}
         .select{coalesce(articles_stock_input_details.name,articles.name).as(article_name)}
         .select{('coalesce(unit_of_measurements_articles.symbol,unit_of_measurements.symbol) AS article_symbol')}
         .select('stock_input_details.amount')
-        .reorder('period, issue_date, article_name')
+        .reorder('cost_center_timelines.period, issue_date, article_name')
     end
   end
 
-  def self.get_kardex_monthly(user, date_type, from_date, to_date)
+  def self.get_kardex_monthly(updatefilter, user, report_type, date_type, from_date, to_date, cost_centers, warehouses, suppliers, years, periods, formats, articles, moneys)
     case date_type
       when "1" # Periodo / Calendar
-        joins{stock_input.warehouse}
+        ActiveRecord::Base.connection.execute("CALL REP_INV_KARDEX_CALENDAR(#{updatefilter}, #{report_type}, 2, #{user}, '#{from_date}', '#{to_date}', '#{cost_centers}', '#{warehouses}', '#{suppliers}', '#{years}', '#{periods}', '#{formats}', '#{articles}', '#{moneys}');")
+      when "2" # TimeLine by Cost Center
+        ActiveRecord::Base.connection.execute("CALL REP_INV_KARDEX_COST_CENTER(#{updatefilter}, #{report_type}, 2, #{user}, '#{from_date}', '#{to_date}', '#{cost_centers}', '#{warehouses}', '#{suppliers}', '#{years}', '#{periods}', '#{formats}', '#{articles}', '#{moneys}');")
+      when "3" # Periodo / Calendar
+      joins{stock_input.warehouse}
+        .joins{stock_input.rep_inv_warehouse}
+        .joins{stock_input.rep_inv_supplier}
+        .joins{stock_input.rep_inv_year}
+        .joins{stock_input.rep_inv_period}
+        .joins{stock_input.rep_inv_format}
         .joins{purchase_order_detail.outer.delivery_order_detail.outer.article.outer.unit_of_measurement.outer}
         .joins{article.outer.unit_of_measurement.outer}
         .joins{article.outer.rep_inv_article.outer}
         .joins{purchase_order_detail.outer.delivery_order_detail.outer.rep_inv_article.outer}
-        .joins{stock_input.rep_inv_warehouse}
-        .joins{stock_input.rep_inv_supplier}
-        .joins{stock_input.rep_inv_period}
-        .joins{stock_input.rep_inv_format}
         .joins{purchase_order_detail.outer.purchase_order.outer.rep_inv_money.outer}
         .where{(rep_inv_warehouses.user.eq "#{user}") &
                (rep_inv_suppliers.user.eq "#{user}") &
+               (rep_inv_years.user.eq "#{user}") &
                (rep_inv_periods.user.eq "#{user}") &
                (rep_inv_formats.user.eq "#{user}") &
                (
@@ -129,21 +151,22 @@ class StockInputDetail < ActiveRecord::Base
         .select{articles.name}
         .select{'unit_of_measurements.symbol'}
         .sum('stock_input_details.amount')
-      when "2" # TimeLine by Cost Center
+      when "4" # TimeLine by Cost Center
         joins{stock_input.warehouse}
-        .joins{stock_input.link_period}
+        .joins{stock_input.link_period.rep_inv_year}
+        .joins{stock_input.link_period.rep_inv_period}
+        .joins{stock_input.rep_inv_warehouse}
+        .joins{stock_input.rep_inv_supplier}
+        .joins{stock_input.rep_inv_format}
         .joins{purchase_order_detail.outer.delivery_order_detail.outer.article.outer.unit_of_measurement.outer}
         .joins{article.outer.unit_of_measurement.outer}
         .joins{article.outer.rep_inv_article.outer}
         .joins{purchase_order_detail.outer.delivery_order_detail.outer.rep_inv_article.outer}
-        .joins{stock_input.rep_inv_warehouse}
-        .joins{stock_input.rep_inv_supplier}
-        .joins{stock_input.rep_inv_period}
-        .joins{stock_input.rep_inv_format}
         .joins{purchase_order_detail.outer.purchase_order.outer.rep_inv_money.outer}
         .where{(link_periods.cost_center_id.eq warehouses.cost_center_id) &
                (rep_inv_warehouses.user.eq "#{user}") &
                (rep_inv_suppliers.user.eq "#{user}") &
+               (rep_inv_years.user.eq "#{user}") &
                (rep_inv_periods.user.eq "#{user}") &
                (rep_inv_formats.user.eq "#{user}") &
                (
@@ -180,21 +203,27 @@ class StockInputDetail < ActiveRecord::Base
     end
   end
 
-  def self.get_kardex_yearly(user, date_type, from_date, to_date)
+  def self.get_kardex_yearly(updatefilter, user, report_type, date_type, from_date, to_date, cost_centers, warehouses, suppliers, years, periods, formats, articles, moneys)
     case date_type
       when "1" # Periodo / Calendar
+      ActiveRecord::Base.connection.execute("CALL REP_INV_KARDEX_CALENDAR(#{updatefilter}, #{report_type}, 1, #{user}, '#{from_date}', '#{to_date}', '#{cost_centers}', '#{warehouses}', '#{suppliers}', '#{years}', '#{periods}', '#{formats}', '#{articles}', '#{moneys}');")
+      when "2" # TimeLine by Cost Center
+        ActiveRecord::Base.connection.execute("CALL REP_INV_KARDEX_COST_CENTER(#{updatefilter}, #{report_type}, 1, #{user}, '#{from_date}', '#{to_date}', '#{cost_centers}', '#{warehouses}', '#{suppliers}', '#{years}', '#{periods}', '#{formats}', '#{articles}', '#{moneys}');")
+      when "3" # Periodo / Calendar
         joins{stock_input.warehouse}
+        .joins{stock_input.rep_inv_warehouse}
+        .joins{stock_input.rep_inv_supplier}
+        .joins{stock_input.rep_inv_year}
+        .joins{stock_input.rep_inv_period}
+        .joins{stock_input.rep_inv_format}
         .joins{purchase_order_detail.outer.delivery_order_detail.outer.article.outer.unit_of_measurement.outer}
         .joins{article.outer.unit_of_measurement.outer}
         .joins{article.outer.rep_inv_article.outer}
         .joins{purchase_order_detail.outer.delivery_order_detail.outer.rep_inv_article.outer}
-        .joins{stock_input.rep_inv_warehouse}
-        .joins{stock_input.rep_inv_supplier}
-        .joins{stock_input.rep_inv_period}
-        .joins{stock_input.rep_inv_format}
         .joins{purchase_order_detail.outer.purchase_order.outer.rep_inv_money.outer}
         .where{(rep_inv_warehouses.user.eq "#{user}") &
                (rep_inv_suppliers.user.eq "#{user}") &
+               (rep_inv_years.user.eq "#{user}") &
                (rep_inv_periods.user.eq "#{user}") &
                (rep_inv_formats.user.eq "#{user}") &
                (
@@ -228,21 +257,22 @@ class StockInputDetail < ActiveRecord::Base
         .select{articles.name}
         .select{'unit_of_measurements.symbol'}
         .sum('stock_input_details.amount')
-      when "2" # TimeLine by Cost Center
+      when "4" # TimeLine by Cost Center
         joins{stock_input.warehouse}
-        .joins{stock_input.link_period}
+        .joins{stock_input.link_period.rep_inv_year}
+        .joins{stock_input.link_period.rep_inv_period}
+        .joins{stock_input.rep_inv_warehouse}
+        .joins{stock_input.rep_inv_supplier}
+        .joins{stock_input.rep_inv_format}
         .joins{purchase_order_detail.outer.delivery_order_detail.outer.article.outer.unit_of_measurement.outer}
         .joins{article.outer.unit_of_measurement.outer}
         .joins{article.outer.rep_inv_article.outer}
         .joins{purchase_order_detail.outer.delivery_order_detail.outer.rep_inv_article.outer}
-        .joins{stock_input.rep_inv_warehouse}
-        .joins{stock_input.rep_inv_supplier}
-        .joins{stock_input.rep_inv_period}
-        .joins{stock_input.rep_inv_format}
         .joins{purchase_order_detail.outer.purchase_order.outer.rep_inv_money.outer}
         .where{(link_periods.cost_center_id.eq warehouses.cost_center_id) &
                (rep_inv_warehouses.user.eq "#{user}") &
                (rep_inv_suppliers.user.eq "#{user}") &
+               (rep_inv_years.user.eq "#{user}") &
                (rep_inv_periods.user.eq "#{user}") &
                (rep_inv_formats.user.eq "#{user}") &
                (
---
+++ sigo/app/views/reports/inventories/show_group_results_pdf.pdf.prawn
@@ -0,0 +1,178 @@
+font "Helvetica"
+
+case @kardex_type
+  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  # Kardex Yearly
+  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  when "1"
+	
+	repeat :all do
+	  font_size 12
+
+	  text "Kárdex Anual", :align => :center
+
+	  font_size 8
+	  move_down 3
+
+	  # header
+	  table([ [{content:"Almacén", rowspan: 2}, {content:"Año", rowspan: 2}, {content:"Código", rowspan: 2}, {content:"Descripción", rowspan: 2}, {content:"Unidad", rowspan: 2},  
+		{content: "Ingreso", colspan: 3}, {content: "Salida", colspan: 3}],
+		["Cantidad", "C/Unitario", "Costo", "Cantidad", "C/Unitario", "Costo"] ], :width => 770, :column_widths => [80,30,60,230,50,50,50,60,50,50,60])  do
+		  style(columns(5..10), :align => :center)
+		end
+
+	  move_down 3
+	end
+
+	item = 0
+	@reportRows.each do |x|
+	  # Page Bring
+	  item += 1
+	  if item == @rows_per_page
+	    start_new_page
+	    move_down 60
+	    item = 1
+	  end
+
+	  # Data
+  	  pad(1) {
+	    table([ ["#{x[1]}", "#{x[2]}", "#{x[6]}", "#{x[7]}", "#{x[8]}", "#{x[9]}", "#{number_with_precision(x[10], precision: 2)}", "#{number_with_precision(x[11], precision: 2, delimiter: ',')}", "#{x[12]}", "#{number_with_precision(x[13], precision: 2)}", "#{number_with_precision(x[14], precision: 2)}"] ], :width => 770, :cell_style => {:border_color=> "ffffff"}, :column_widths => [80,30,60,230,50,50,50,60,50,50,60]) do
+	      style(columns(1..2), :align => :center)
+	      style(columns(5..10), :align => :right)
+	    end
+	  }
+	
+	  stroke_horizontal_rule
+  end
+
+  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  # Kardex Monthly
+  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  when "2"
+    repeat :all do
+	  font_size 12
+
+	  text "Kárdex Mensual", :align => :center
+
+	  font_size 8
+	  move_down 3
+
+	  table([ [{content:"Almacén", rowspan: 2}, {content:"Periodo", rowspan: 2}, {content:"Código", rowspan: 2}, {content:"Descripción", rowspan: 2}, {content:"Unidad", rowspan: 2},  
+	{content: "Ingreso", colspan: 3}, {content: "Salida", colspan: 3}],
+	["Cantidad", "C/Unitario", "Costo", "Cantidad", "C/Unitario", "Costo"] ], :width => 770, :column_widths => [80,40,60,220,50,50,50,60,50,50,60])  do
+	  style(columns(5..10), :align => :center)
+	  end
+
+	  move_down 3
+	end
+
+	item = 0
+	@reportRows.each do |x|
+	  # Page Bring
+	  item += 1
+	  if item == @rows_per_page
+	    start_new_page
+	    move_down 60
+	    item = 1
+	  end
+	
+	  # Data
+  	  pad(1) {
+	    table([ ["#{x[1]}", "#{x[3]}", "#{x[6]}", "#{x[7]}", "#{x[8]}", "#{x[9]}", "#{number_with_precision(x[10], precision: 2, delimiter: ',')}", "#{number_with_precision(x[11], precision: 2, delimiter: ',')}", "#{x[12]}", "#{number_with_precision(x[13], precision: 2, delimiter: ',')}", "#{number_with_precision(x[14], precision: 2, delimiter: ',')}"] ], :width => 770, :cell_style => {:border_color=> "ffffff"}, :column_widths => [80,40,60,220,50,50,50,60,50,50,60]) do
+	      style(columns(1..2), :align => :center)
+	      style(columns(5..10), :align => :right)
+	    end
+	  }
+
+	  stroke_horizontal_rule
+	end
+
+  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  # Kardex Daily
+  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  when "3"
+	repeat :all do
+	  font_size 12
+
+	  text "Kárdex Detallado", :align => :center
+
+	  font_size 8
+	  move_down 3
+
+	  table([ [{content:"Almacén", rowspan: 2}, {content:"Periodo", rowspan: 2}, {content:"Fecha", rowspan: 2}, {content:"Código", rowspan: 2}, {content:"Descripción", rowspan: 2}, {content:"Unidad", rowspan: 2},  
+	{content: "Ingreso", colspan: 3}, {content: "Salida", colspan: 3}],
+	["Cantidad", "C/Unitario", "Costo", "Cantidad", "C/Unitario", "Costo"] ], :width => 770, :column_widths => [80,40,60,60,170,50,50,50,50,50,50,60])  do
+	  style(columns(5..10), :align => :center)
+	  end
+
+	  move_down 3
+	end
+
+	item = 0
+	@reportRows.each do |x|
+	  # Page Bring
+	  item += 1
+	  if item == @rows_per_page
+	    start_new_page
+	    move_down 60
+	    item = 1
+	  end
+
+	  # Data
+  	  pad(1) {
+	    table([ ["#{x[1]}", "#{x[3]}", "#{x[4]}", "#{x[6]}", "#{x[7]}", "#{x[8]}", "#{x[9]}", "#{number_with_precision(x[10], precision: 2, delimiter: ',')}", "#{number_with_precision(x[11], precision: 2, delimiter: ',')}", "#{x[12]}", "#{number_with_precision(x[13], precision: 2, delimiter: ',')}", "#{number_with_precision(x[14], precision: 2, delimiter: ',')}"] ], :width => 770, :cell_style => {:border_color=> "ffffff"}, :column_widths => [80,40,60,60,170,50,50,50,50,50,50,60]) do
+	      style(columns(1..2), :align => :center)
+	      style(columns(5..11), :align => :right)
+	    end
+	  }
+
+	  stroke_horizontal_rule
+	end
+  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  # Kardex Resumen
+  #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  when "4"
+	repeat :all do
+	  font_size 12
+
+	  text "Kárdex Detallado", :align => :center
+
+	  font_size 8
+	  move_down 3
+
+	  table([ [{content:"Almacén", rowspan: 2}, {content:"Código", rowspan: 2}, {content:"Descripción", rowspan: 2}, {content:"Unidad", rowspan: 2},  
+	{content: "Ingreso", colspan: 3}, {content: "Salida", colspan: 3}],
+	["Cantidad", "C/Unitario", "Costo", "Cantidad", "C/Unitario", "Costo"] ], :width => 770, :column_widths => [80,60,270,60,50,50,50,50,50,50])  do
+	  style(columns(5..10), :align => :center)
+	  end
+
+	  move_down 3
+	end
+
+	item = 0
+	@reportRows.each do |x|
+	  # Page Bring
+	  item += 1
+	  if item == @rows_per_page
+	    start_new_page
+	    move_down 60
+	    item = 1
+	  end
+
+	  # Data
+  	  pad(1) {
+	    table([ ["#{x[1]}", "#{x[6]}", "#{x[7]}", "#{x[8]}", "#{x[9]}", "#{number_with_precision(x[10], precision: 2, delimiter: ',')}", "#{number_with_precision(x[11], precision: 2, delimiter: ',')}", "#{x[12]}", "#{number_with_precision(x[13], precision: 2, delimiter: ',')}", "#{number_with_precision(x[14], precision: 2, delimiter: ',')}"] ], :width => 770, :cell_style => {:border_color=> "ffffff"}, :column_widths => [80,60,270,60,50,50,50,50,50,50]) do
+	      style(columns(1..1), :align => :center)
+	      style(columns(4..10), :align => :right)
+	    end
+	  }
+
+	  stroke_horizontal_rule
+	end
+end
+
+# footer
+page_count.times do |i|
+  go_to_page(i+1)
+  text "#{i+1} / #{page_count}", :align => :right
+end
