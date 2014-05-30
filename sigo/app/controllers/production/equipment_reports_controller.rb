357444a55d2fb4dd3282a4b1851e89bf333d43e2
Giancarlo Guzman <giancarloguzo@gmail.com>
2014-05-30 09:08:01 -0500

    reporte de equipos

--- sigo/app/controllers/production/equipment_reports_controller.rb
+++ sigo/app/controllers/production/equipment_reports_controller.rb
@@ -76,8 +76,8 @@ class Production::EquipmentReportsController < ApplicationController
     poe_array2 = ActiveRecord::Base.connection.execute("
       SELECT pha.id, pha.name , SUM(poed.effective_hours) , SUM(poe.fuel_amount), ROUND((SUM(poe.fuel_amount)/SUM(poed.effective_hours)), 2) 
 			FROM part_of_equipments poe, workers wo, part_of_equipment_details poed,phases pha,subcontract_equipment_details sced 
-			WHERE sced.code IN(123) 
-			AND poe.date BETWEEN '2014-05-01' AND '2014-05-10'
+			WHERE sced.code IN(" + working_group_id + ")
+			AND poe.date BETWEEN '" + start_date + "' AND '" + end_date + "'
 			AND poe.worker_id=wo.id 
 			AND poe.equipment_id=sced.article_id 
 			AND poe.id=poed.part_of_equipment_id 
