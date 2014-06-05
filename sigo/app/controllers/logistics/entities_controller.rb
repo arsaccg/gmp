2f85b4007c2e715a9566252da7244623562df4c2
Antonny Aguilar <antonny.aguilarc@gmail.com>
2014-06-05 10:53:06 -0500

    entidad edit

--- sigo/app/controllers/logistics/entities_controller.rb
+++ sigo/app/controllers/logistics/entities_controller.rb
@@ -38,10 +38,10 @@ class Logistics::EntitiesController < ApplicationController
   end
 
   def edit
-    @company_id = params[:company_id]
+    @company_id = get_company_cost_center('company')
     @reg_ed = Time.now.to_i
     @entity = Entity.find(params[:id])
-    @costCenter = Company.find(params[:company_id]).cost_centers
+    @costCenter = Company.find(@company_id).cost_centers
     @all_entity_types = Array.new
     @own_entity_types = Array.new
     # Comenzando lógica
