c0ac6c992c0ccf95edea8dcb90902500f882c7a0
Giancarlo Guzman <giancarloguzo@gmail.com>
2014-06-06 17:34:05 -0500

    reportes

--- sigo/app/controllers/reports/reportofthemonths_controller.rb
+++ sigo/app/controllers/reports/reportofthemonths_controller.rb
@@ -6,26 +6,52 @@ class Reports::ReportofthemonthsController < ApplicationController
   	@dia = @d.at_beginning_of_month.strftime
   	@dia2 = @d.at_end_of_month.strftime
     @cost_center = get_company_cost_center('cost_center')
+
 		@part_work = part_work(@dia, @dia2, @cost_center)
 		@part_work.each do |workerDetail|
       @partwork = workerDetail[0]
     end
+    @part_work2 = part_work2(@dia, @dia2, @cost_center)
+		@part_work2.each do |workerDetail|
+      @partwork2 = workerDetail[0]
+    end
+
 		@part_equipment = part_equipment(@dia, @dia2, @cost_center)
 		@part_equipment.each do |workerDetail|
       @partequipment = workerDetail[0]
     end
+    @part_equipment2 = part_equipment2(@dia, @dia2, @cost_center)
+		@part_equipment2.each do |workerDetail|
+      @partequipment2 = workerDetail[0]
+    end
+
 		@part_people = part_people(@dia, @dia2, @cost_center)
 		@part_people.each do |workerDetail|
       @partpeople = workerDetail[0]
     end
+    @part_people2 = part_people2(@dia, @dia2, @cost_center)
+		@part_people2.each do |workerDetail|
+      @partpeople2 = workerDetail[0]
+    end
+
 		@order_of_service = order_of_service(@dia, @dia2, @cost_center)
 		@order_of_service.each do |workerDetail|
       @orderofservice = workerDetail[0]
     end
+    @order_of_service2 = order_of_service2(@dia, @dia2, @cost_center)
+		@order_of_service2.each do |workerDetail|
+      @orderofservice2 = workerDetail[0]
+    end
+
 		@sc_valuations = sc_valuations(@dia, @dia2, @cost_center)
 		@sc_valuations.each do |workerDetail|
       @scvaluations = workerDetail[0]
     end
+    @sc_valuations2 = sc_valuations2(@dia, @dia2, @cost_center)
+		@sc_valuations2.each do |workerDetail|
+      @scvaluations2 = workerDetail[0]
+    end
+
 		render layout: false
   end
 
@@ -46,7 +72,7 @@ class Reports::ReportofthemonthsController < ApplicationController
     workers_array3 = ActiveRecord::Base.connection.execute("
       SELECT SUM(pwd.bill_of_quantitties*si.price) AS PartWork 
 			FROM part_works pw, part_work_details pwd, articles art,subcontract_inputs si 
-			WHERE pw.date_of_creation BETWEEN '" + start_date + "' AND '" + end_date + "'
+			WHERE pw.date_of_creation < '" + end_date + "'
 			AND pw.id = pwd.part_work_id
 			AND pwd.article_id = art.id
 			AND si.article_id = pwd.article_id
@@ -59,7 +85,7 @@ class Reports::ReportofthemonthsController < ApplicationController
     workers_array3 = ActiveRecord::Base.connection.execute("
       SELECT SUM(poed.effective_hours*si.price) AS PartEquip
 			FROM part_of_equipments poe, part_of_equipment_details poed, articles art,subcontract_inputs si 
-			WHERE poe.date BETWEEN '2014-01-1' AND '2014-01-31'
+			WHERE poe.date BETWEEN '" + start_date + "' AND '" + end_date + "'
 			AND poe.id = poed.part_of_equipment_id
 			AND poe.equipment_id = art.id
 			AND si.article_id = poe.equipment_id
@@ -70,12 +96,12 @@ class Reports::ReportofthemonthsController < ApplicationController
 
   def part_equipment2(start_date, end_date, working_group_id)
     workers_array3 = ActiveRecord::Base.connection.execute("
-      SELECT SUM(pwd.bill_of_quantitties*si.price) AS PartWork 
-			FROM part_works pw, part_work_details pwd, articles art,subcontract_inputs si 
-			WHERE pw.date_of_creation BETWEEN '" + start_date + "' AND '" + end_date + "'
-			AND pw.id = pwd.part_work_id
-			AND pwd.article_id = art.id
-			AND si.article_id = pwd.article_id
+      SELECT SUM(poed.effective_hours*si.price) AS PartEquip
+			FROM part_of_equipments poe, part_of_equipment_details poed, articles art,subcontract_inputs si 
+			WHERE poe.date < '" + end_date + "'
+			AND poe.id = poed.part_of_equipment_id
+			AND poe.equipment_id = art.id
+			AND si.article_id = poe.equipment_id
       AND poe.cost_center_id IN(" + working_group_id + ")
     ")
     return workers_array3
@@ -99,7 +125,7 @@ class Reports::ReportofthemonthsController < ApplicationController
     workers_array3 = ActiveRecord::Base.connection.execute("
       SELECT SUM((ppd.normal_hours*cow.normal_price)+(ppd.he_60*cow.he_60_price)+(ppd.he_100*cow.he_100_price)) AS PartPerson
 			FROM part_people pp, part_person_details ppd, articles art, workers wo, category_of_workers cow
-			WHERE pp.date_of_creation BETWEEN '" + start_date + "' AND '" + end_date + "'
+			WHERE pp.date_of_creation < '" + end_date + "'
 			AND pp.id = ppd.part_person_id
 			AND ppd.worker_id = wo.id
 			AND wo.article_id = art.id
@@ -125,7 +151,7 @@ class Reports::ReportofthemonthsController < ApplicationController
       SELECT SUM(oosd.unit_price_igv)
 			FROM order_of_services oos, order_of_service_details oosd
 			WHERE oosd.order_of_service_id = oos.id
-			AND oos.date_of_service BETWEEN '" + start_date + "' AND '" + end_date + "'
+			AND oos.date_of_service < '" + end_date + "'
 			AND oos.cost_center_id IN(" + working_group_id + ")
     ")
     return workers_array3
@@ -145,8 +171,7 @@ class Reports::ReportofthemonthsController < ApplicationController
     workers_array3 = ActiveRecord::Base.connection.execute("
       SELECT SUM(net_payment) AS SCVALUATION
 			FROM  sc_valuations
-			WHERE start_date BETWEEN '" + start_date + "' AND '" + end_date + "'
-			AND end_date BETWEEN '" + start_date + "' AND '" + end_date + "'
+			WHERE end_date < '" + end_date + "'
     ")
     return workers_array3
   end
