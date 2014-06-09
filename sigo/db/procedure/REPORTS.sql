4e31315a58a87e62002611daa0b2bdbb0f3b3dd3
Eduardo Cabrera <dvex.cabrera@gmail.com>
2014-06-09 17:51:21 -0500

    Function de venta valorizada

--- sigo/db/procedure/REPORTS.sql
+++ sigo/db/procedure/REPORTS.sql
@@ -39,78 +39,62 @@ BEGIN
   RETURN ROUND(v_sum, 2);
 END$$
 
-DROP FUNCTION IF EXISTS `get_valorization_actual`$$
-CREATE DEFINER=`root`@`localhost` FUNCTION `get_amount_actual`( type_article CHAR(2), v_order TEXT, v_budget_id INTEGER, v_valorization_id INTEGER) RETURNS float
+
+DROP FUNCTION IF EXISTS `get_valorized_sale`$$
+CREATE DEFINER=`root`@`localhost` FUNCTION `get_valorized_sale`( type_article INT, cost_center_id INT ) RETURNS FLOAT
 BEGIN
-
   DECLARE done INT DEFAULT FALSE;
-
-  DECLARE v_id FLOAT;
-  DECLARE v_price FLOAT;
-  DECLARE v_created_at DATETIME;
-  DECLARE v_total FLOAT;
-
+  DECLARE v_budget_id INT; 
+  DECLARE v_itembybudget_id INT; 
+  DECLARE v_actual_measured FLOAT;
   DECLARE i_measured FLOAT;
- 
-  DECLARE sub_elements CURSOR FOR 
-    SELECT id, price FROM itembybudgets WHERE `order` LIKE CONVERT(CONCAT(v_order, "%") using utf8) collate utf8_spanish_ci AND budget_id = v_budget_id;
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-  
-  SET v_total=0;
 
-  OPEN sub_elements;
-  read_loop: LOOP
-    FETCH sub_elements INTO v_id, v_price;
-    
-    SET @i_measured = (SELECT actual_measured FROM valorizationitems WHERE valorization_id = v_valorization_id AND itembybudget_id = v_id LIMIT 1);
-    SET i_measured  = (SELECT @i_measured);
- 
+  DECLARE measured_cursor CURSOR FOR 
+    SELECT b.id, vi.itembybudget_id, vi.actual_measured 
+    FROM `budgets` b, `itembybudgets` ibb, `valorizationitems` vi 
+  WHERE b.type_of_budget = 1 
+  AND b.cost_center_id = cost_center_id 
+  AND ibb.budget_id = b.id 
+  AND vi.itembybudget_id = ibb.id;
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+  OPEN measured_cursor;
+
+    read_loop: LOOP
+    FETCH measured_cursor INTO v_budget_id, v_itembybudget_id, v_actual_measured;
+
     IF done THEN
       LEAVE read_loop;
     END IF;
-    SET v_total = v_total + (IFNULL(v_price, 0) * IFNULL(i_measured, 0));
-  END LOOP;
-  CLOSE sub_elements;
-  
-  RETURN v_total;
-END$$
 
-DROP FUNCTION IF EXISTS `get_valorization_acumulated`$$
-CREATE DEFINER=`root`@`localhost` FUNCTION `get_amount_acumulated`(type_article CHAR(2), v_order TEXT, v_budget_id INTEGER, v_current_date DATETIME, v_valorization_id INTEGER) RETURNS float
-BEGIN
+      SET @i_measured = (
+    SELECT DISTINCT SUM(items.price * items.quantity * items.measured)
+    FROM inputcategories,
+    (
+      SELECT  inputs.cod_input, inputs.quantity, inputs.price, itembybudgets.measured, inputs.item_id, inputs.`order`
+      FROM itembybudgets,
+      (
+        SELECT quantity, price, `order`, item_id, cod_input, budget_id
+        FROM inputbybudgetanditems
+      ) AS inputs
+      WHERE inputs.item_id = itembybudgets.item_id AND
+        inputs.`order` = itembybudgets.`order` AND
+        inputs.budget_id = itembybudgets.budget_id AND
+        itembybudgets.budget_id = v_budget_id AND
+        itembybudgets.id = v_itembybudget_id
+    ) AS items
+    WHERE items.cod_input LIKE CONCAT('0', CONCAT(type_article, '%'))
+    GROUP BY category_id
+    ORDER BY category_id
+      );
 
-  DECLARE done INT DEFAULT FALSE;
+      SET i_measured  = i_measured + (SELECT @i_measured);
 
-  DECLARE v_id FLOAT;
-  DECLARE v_price FLOAT;
-  DECLARE v_created_at DATETIME;
-  DECLARE v_total FLOAT;
+    END LOOP;
 
-  DECLARE i_measured FLOAT;
-  DECLARE i_prev_measured FLOAT;
+  CLOSE measured_cursor;
 
-  DECLARE sub_elements CURSOR FOR 
-    SELECT id, price, created_at FROM itembybudgets WHERE `order` LIKE CONVERT(CONCAT(v_order, "%") using utf8) collate utf8_spanish_ci AND budget_id = v_budget_id;
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-  
-  SET v_total=0;
-
-  OPEN sub_elements;
-  read_loop: LOOP
-    FETCH sub_elements INTO v_id, v_price, v_created_at;
-    
-    SET @i_measured = (SELECT actual_measured FROM valorizationitems WHERE valorization_id = v_valorization_id AND itembybudget_id = v_id LIMIT 1);
-    SET i_measured  = (SELECT @i_measured);
-    SET i_prev_measured = (SELECT get_prev_valorizations(v_created_at, v_id));
-  
-    IF done THEN
-      LEAVE read_loop;
-    END IF;
-    SET v_total = v_total + (IFNULL(v_price, 0) * IFNULL(i_measured, 0)) + (IFNULL(i_prev_measured, 0) * IFNULL(v_price, 0));
-  END LOOP;
-  CLOSE sub_elements;
-  
-  RETURN v_total;
+  RETURN ROUND(i_measured, 2);
 END$$
 
 DELIMITER ;
