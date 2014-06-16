3724412a096048c7b18745f94d6fd49926600da2
Eduardo Cabrera <dvex.cabrera@gmail.com>
2014-06-16 16:04:54 -0500

    Modificaciones a la funcion get_partial_cost procedure

--- tobi/app/controllers/management/distributions_controller.rb
+++ tobi/app/controllers/management/distributions_controller.rb
@@ -3,9 +3,11 @@ class Management::DistributionsController < ApplicationController
   
   def import_distributions
     @cost_center_id = params[:project_id]
+    @distribution = Distribution.select(:code).where('cost_center_id = ?', @cost_center_id).first()
   end
   
   def do_import
     Distribution.import_data_from_excel(params[:file], params[:cost_center_id], params[:quantity])
+    redirect_to :action => :import_distributions
   end
 end
