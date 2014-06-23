cbcc2dd0f596f3d0299197b84ba307d76cf6c8da
Eduardo Cabrera <dvex.cabrera@gmail.com>
2014-06-23 16:19:10 -0500

    Eliminando vistas x las..

--- sigo/app/models/distribution.rb
+++ sigo/app/models/distribution.rb
@@ -43,7 +43,7 @@ class Distribution < ActiveRecord::Base
   
   def self.open_spreadsheet(file)
     case File.extname(file.original_filename)
-    when ".csv" then Roo::CSV.new(file.path , csv_options: {encoding:Encoding::ISO_8859_1, :col_sep => ";"})
+    when ".csv" then Roo::CSV.new(file.path , csv_options: {encoding:Encoding::ISO_8859_1, :col_sep => ","})
     when ".xls" then Roo::Excel.new(file.path)
     when ".xlsx" then Roo::Excelx.new(file.path)
     else raise "Unknown file type: #{file.original_filename}"
