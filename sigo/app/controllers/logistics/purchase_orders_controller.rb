78fb5f1be513df90a89f0a3e2cec19a8990eff06
Antonny Aguilar <antonny.aguilarc@gmail.com>
2014-06-25 10:41:30 -0500

    purchase

--- sigo/app/controllers/logistics/purchase_orders_controller.rb
+++ sigo/app/controllers/logistics/purchase_orders_controller.rb
@@ -1,4 +1,5 @@
 class Logistics::PurchaseOrdersController < ApplicationController
+  protect_from_forgery with: :null_session, :only => [:destroy, :delete]
   def index
     @company = params[:company_id]
     @company = get_company_cost_center('company')
