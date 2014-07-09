7a39fd3682db8527241a6f3c2457b3975caed6b5
Giancarlo Guzman <giancarloguzo@gmail.com>
2014-07-09 16:01:24 -0500

    fixum into string

--- sigo/app/models/cost_center.rb
+++ sigo/app/models/cost_center.rb
@@ -52,7 +52,7 @@ class CostCenter < ActiveRecord::Base
   def self.getWeek(cost_center_id,end_date)
     week_array = ActiveRecord::Base.connection.execute("
       SELECT id,name,start_date,end_date
-      FROM weeks_for_cost_center_" + cost_center_id + " 
+      FROM weeks_for_cost_center_" + cost_center_id.to_s + " 
       WHERE start_date < '" + end_date + "'
     ")    
   end
