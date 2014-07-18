484046b99e009bace37e89abdf4cb532aa747e7b
Eduardo Cabrera <dvex.cabrera@gmail.com>
2014-07-18 10:33:24 -0500

    Cuenta Contable y Sub Diarios completados

--- sigo/app/controllers/administration/account_accountants_controller.rb
+++ sigo/app/controllers/administration/account_accountants_controller.rb
@@ -1,20 +1,54 @@
 class Administration::AccountAccountantsController < ApplicationController
   def index
+    @accountAccountants = AccountAccountant.all
+    render layout: false
   end
 
   def new
+    @accountAccountant = AccountAccountant.new
+    render :new, layout: false
   end
 
   def create
+    accountAccountant = AccountAccountant.new(account_accountant_parameters)
+    if accountAccountant.save
+      flash[:notice] = "Se ha creado correctamente cuenta contable."
+      redirect_to :action => :index
+    else
+      accountAccountant.errors.messages.each do |attribute, error|
+        flash[:error] =  flash[:error].to_s + error.to_s + "  "
+      end
+      # Load new()
+      @accountAccountant = accountAccountant
+      render :new, layout: false
+    end
   end
 
   def edit
+    @accountAccountant = AccountAccountant.find(params[:id])
+    @action = 'edit'
+    render layout: false
   end
 
   def update
+    accountAccountant = AccountAccountant.find(params[:id])
+    if accountAccountant.update_attributes(account_accountant_parameters)
+      flash[:notice] = "Se ha actualizado correctamente los datos."
+      redirect_to :action => :index
+    else
+      accountAccountant.errors.messages.each do |attribute, error|
+        flash[:error] =  flash[:error].to_s + error.to_s + "  "
+      end
+      # Load new()
+      @accountAccountant = accountAccountant
+      render :edit, layout: false
+    end
   end
 
   def destroy
+    accountAccountant = AccountAccountant.destroy(params[:id])
+    flash[:notice] = "Se ha eliminado correctamente."
+    render :json => accountAccountant
   end
 
   def import
--- sigo/app/controllers/administration/sub_dailies_controller.rb
+++ sigo/app/controllers/administration/sub_dailies_controller.rb
@@ -1,20 +1,54 @@
 class Administration::SubDailiesController < ApplicationController
   def index
+    @subDailies = SubDaily.all
+    render layout: false
   end
 
   def new
+    @subDaily = SubDaily.new
+    render :new, layout: false
   end
 
   def create
+    subDaily = SubDaily.new(sub_daily_parameters)
+    if subDaily.save
+      flash[:notice] = "Se ha creado correctamente el nuevo Sub-Diario."
+      redirect_to :action => :index
+    else
+      subDaily.errors.messages.each do |attribute, error|
+        flash[:error] =  flash[:error].to_s + error.to_s + "  "
+      end
+      # Load new()
+      @subDaily = subDaily
+      render :new, layout: false
+    end
   end
 
   def edit
+    @subDaily = SubDaily.find(params[:id])
+    @action = 'edit'
+    render layout: false
   end
 
   def update
+    subDaily = SubDaily.find(params[:id])
+    if subDaily.update_attributes(sub_daily_parameters)
+      flash[:notice] = "Se ha actualizado correctamente los datos."
+      redirect_to :action => :index
+    else
+      subDaily.errors.messages.each do |attribute, error|
+        flash[:error] =  flash[:error].to_s + error.to_s + "  "
+      end
+      # Load new()
+      @subDaily = subDaily
+      render :edit, layout: false
+    end
   end
 
   def destroy
+    subDaily = SubDaily.destroy(params[:id])
+    flash[:notice] = "Se ha eliminado correctamente."
+    render :json => subDaily
   end
 
   def import
