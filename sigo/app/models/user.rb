883eb9d69c077f4a09fd6049213cdb49a05e420f
Eduardo Cabrera <dvex.cabrera@gmail.com>
2014-07-24 08:49:41 -0500

    Retomando el RoleModel que fue borrado

--- sigo/app/models/user.rb
+++ sigo/app/models/user.rb
@@ -18,7 +18,8 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "28x25>" }, :default_url => "/assets/images/avatars/male.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   
-  #roles :director, :approver, :reviser, :issuer, :canceller
+  include RoleModel
+  roles :director, :approver, :reviser, :issuer, :canceller
 
   # Director = Can create CIA and Users
   # Anuler = Can cancel orders
