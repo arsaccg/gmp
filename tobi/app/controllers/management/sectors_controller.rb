26e5d3047fd6627e4917927cac38b9742d8bc19f
Joel Ibaceta <joelibaceta@MacBook-Pro-de-JoelMacBook-MacBook-Pro-de-happy-girlhaPro-de-Jo.local>
2014-07-24 14:44:09 -0500

    data

---
+++ tobi/app/controllers/management/sectors_controller.rb
@@ -0,0 +1,36 @@
+class Management::SectorsController < ApplicationController
+  
+  before_filter :authorize_manager
+
+  def index 
+
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def new
+
+  end
+
+  def set_sectors_by_cost_center
+  	cost_center = CostCenter.find(params[:id])
+  	@sectors = cost_center.sectors
+  	@items = cost_center.items
+  	render 'set_sector_cc', layout: false
+  end
+
+  def update_sector
+
+  end
+
+end
---
+++ tobi/test/models/measured_by_sector_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MeasuredBySectorTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
