0a3c2b348b9fe0c116127250a74a4c0fb7a1ce82
Antonny Aguilar <antonny.aguilarc@gmail.com>
2014-08-08 12:27:00 -0500

    modificaciones control documentario

--- sigo/db/migrate/20140725160859_create_general_expenses.rb
+++ sigo/db/migrate/20140725160859_create_general_expenses.rb
@@ -1,10 +1,12 @@
 class CreateGeneralExpenses < ActiveRecord::Migration
   def change
-    create_table :general_expenses do |t|
-      t.integer :phase_id
-      t.integer :cost_center_id
+  	if !table_exists? :general_expenses
+	    create_table :general_expenses do |t|
+	      t.integer :phase_id
+	      t.integer :cost_center_id
 
-      t.timestamps
-    end
+	      t.timestamps
+	    end
+	  end
   end
 end
--- sigo/db/migrate/20140725232020_addfield_to_worker_and_contracts.rb
+++ sigo/db/migrate/20140725232020_addfield_to_worker_and_contracts.rb
@@ -1,7 +1,6 @@
 class AddfieldToWorkerAndContracts < ActiveRecord::Migration
   def change
     add_column :workers, :afpnumber, :string
-  	remove_column :worker_afps, :afpnumber
     add_column :worker_contracts, :contract_type_id, :integer
   end
 end
