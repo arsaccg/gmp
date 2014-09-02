adda0d8c0bc7756069d479a77a9262f3130b7c60
Giancarlo Guzman <giancarloguzo@gmail.com>
2014-09-02 15:26:06 -0500

    fixes budget y valorizations

--- tobi/app/controllers/management/budgets_controller.rb
+++ tobi/app/controllers/management/budgets_controller.rb
@@ -13,6 +13,8 @@ class Management::BudgetsController < ApplicationController
     @project_id = params[:project_id]
     @budgets_goal = Budget.where("type_of_budget = ? AND cost_center_id = ? ", '0', @project_id)
     @budgets_sale = Budget.where("type_of_budget = ? AND cost_center_id = ? ", '1', @project_id) 
+    ActiveRecord::Base.connection.execute('UPDATE itembybudgets SET subbudgetdetail = (SELECT description FROM items WHERE item_code = itembybudgets.item_code LIMIT 1)  WHERE title="REGISTRO RESTRINGIDO" AND subbudgetdetail = "" AND itembybudgets.budget_id = ' + @budgets_goal.id.to_s + ';')
+
     render :get_budget_by_project, layout: false
   end
   
