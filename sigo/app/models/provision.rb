65ba7dfa4563549bd7bd3c8636ee90743d7d0348
Eduardo Cabrera <dvex.cabrera@gmail.com>
2014-10-14 17:29:15 -0500

    Terminando provisiones, cambios y correcciones

--- sigo/app/models/provision.rb
+++ sigo/app/models/provision.rb
@@ -1,7 +1,7 @@
 class Provision < ActiveRecord::Base
   belongs_to :cost_center
   belongs_to :document_provision
-  belongs_to :payment_order
+  has_one :payment_order
   belongs_to :entity
   has_many :provision_details
   has_many :provision_direct_purchase_details
