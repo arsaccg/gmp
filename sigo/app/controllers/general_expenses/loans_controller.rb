6a6742b504419d9f12082bdb44f8ca9bb21d1f2a
Steven Torres <steventorrestorreblanca@hotmail.com>
2014-10-15 15:26:05 -0500

    cambios

--- sigo/app/controllers/general_expenses/loans_controller.rb
+++ sigo/app/controllers/general_expenses/loans_controller.rb
@@ -26,7 +26,7 @@ class GeneralExpenses::LoansController < ApplicationController
     loan.state = params[:state]
     loan.refund_date = params[:refund_date]
     loan.cost_center_beneficiary_id = params[:cost_center_beneficiary_id]
-    loan.cost_center_lender_id = params[:cc_id]
+    loan.cost_center_lender_id = params[:cost_center_lender_id]
     if loan.save
       loan.errors.messages.each do |attribute, error|
         flash[:error] =  flash[:error].to_s + error.to_s + "  "
@@ -66,7 +66,8 @@ class GeneralExpenses::LoansController < ApplicationController
 
   def index
     @cc = CostCenter.find(get_company_cost_center('cost_center'))
-    @loan = Loan.where('cost_center_lender_id=?', @cc)
+    @presto = Loan.where('cost_center_lender_id=?', @cc.id)
+    @prestaron = Loan.where('cost_center_beneficiary_id=?', @cc.id)
     render layout: false
   end
 
