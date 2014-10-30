0d82641f38ccd8e7df4bb243cd58fcb45a897207
Eduardo Cabrera <dvex.cabrera@gmail.com>
2014-10-30 16:45:34 -0500

    Fixes en provisiones

--- sigo/app/models/provision_detail.rb
+++ sigo/app/models/provision_detail.rb
@@ -3,5 +3,5 @@ class ProvisionDetail < ActiveRecord::Base
 	belongs_to :account_accountant
 	has_many :provision_direct_purchase_detail
 
-  	accepts_nested_attributes_for :provision_direct_purchase_details, :allow_destroy => true
+  	#accepts_nested_attributes_for :provision_direct_purchase_details, :allow_destroy => true
 end
