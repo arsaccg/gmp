5237840a3e552b59756b69c18ea801d643aafead
Antonny Aguilar <antonny.aguilarc@gmail.com>
2014-10-30 16:02:34 -0500

    show provision

--- sigo/app/models/provision_direct_purchase_detail.rb
+++ sigo/app/models/provision_direct_purchase_detail.rb
@@ -2,6 +2,7 @@ class ProvisionDirectPurchaseDetail < ActiveRecord::Base
 	belongs_to :provision, :touch => true
 	belongs_to :account_accountant
 	has_many :provision_direct_extra_calculations
+	belongs_to :article
 
 	accepts_nested_attributes_for :provision_direct_extra_calculations, :allow_destroy => true	
 end
