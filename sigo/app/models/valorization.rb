0cd132119ccad818e07bebbae3a84aec8ea29716
Giancarlo Guzman <giancarloguzo@gmail.com>
2014-11-03 16:58:05 -0500

    valorizations

--- sigo/app/models/valorization.rb
+++ sigo/app/models/valorization.rb
@@ -12,7 +12,7 @@ class Valorization < ActiveRecord::Base
       end
     end
 
-  def self.get_sub_itembybudgets(orderitem, valorization_id)
+  def self.get_sub_itembybudgets(orderitem, valorization_id, budget_id)
     
     orderi = orderitem + '%'
     
@@ -22,15 +22,15 @@ class Valorization < ActiveRecord::Base
         subbudgetdetail, 
         'UND', price, measured, 
         (price * measured) AS 'total', 
-        IFNULL(get_prev_valorizations(valorizations.created_at, itembybudgets.id), 0) AS 'metrado_acumulado_anterior', 
-        IFNULL((get_prev_valorizations(valorizations.created_at, itembybudgets.id) * price), 0) AS 'costo_acumulado_anterior', 
+        IFNULL(get_prev_valorizations(valorizations.valorization_date, itembybudgets.id), 0) AS 'metrado_acumulado_anterior', 
+        IFNULL((get_prev_valorizations(valorizations.valorization_date, itembybudgets.id) * price), 0) AS 'costo_acumulado_anterior', 
         valorizationitems.actual_measured, 
         IFNULL((valorizationitems.actual_measured * price), 0) AS 'costo_actual',
-        IFNULL((get_prev_valorizations(valorizations.created_at, itembybudgets.id) + valorizationitems.actual_measured), 0) as 'metrado acumulado', 
-        IFNULL((get_prev_valorizations(valorizations.created_at, itembybudgets.id) + valorizationitems.actual_measured) * price, 0) as 'costo acumulado',
-        IFNULL(measured - (get_prev_valorizations(valorizations.created_at, itembybudgets.id) + valorizationitems.actual_measured), 0) AS 'saldo_metrado', 
-        IFNULL((measured - (get_prev_valorizations(valorizations.created_at, itembybudgets.id) + valorizationitems.actual_measured)) * price, 0) AS 'saldo_costo',
-        IFNULL(((get_prev_valorizations(valorizations.created_at, itembybudgets.id) + valorizationitems.actual_measured) / measured) * 100, 0) AS 'avance'
+        IFNULL((get_prev_valorizations(valorizations.valorization_date, itembybudgets.id) + valorizationitems.actual_measured), 0) as 'metrado acumulado', 
+        IFNULL((get_prev_valorizations(valorizations.valorization_date, itembybudgets.id) + valorizationitems.actual_measured) * price, 0) as 'costo acumulado',
+        IFNULL(measured - (get_prev_valorizations(valorizations.valorization_date, itembybudgets.id) + valorizationitems.actual_measured), 0) AS 'saldo_metrado', 
+        IFNULL((measured - (get_prev_valorizations(valorizations.valorization_date, itembybudgets.id) + valorizationitems.actual_measured)) * price, 0) AS 'saldo_costo',
+        IFNULL(((get_prev_valorizations(valorizations.valorization_date, itembybudgets.id) + valorizationitems.actual_measured) / measured) * 100, 0) AS 'avance'
         FROM     `itembybudgets` LEFT JOIN
         valorizationitems ON valorizationitems.itembybudget_id = itembybudgets.id LEFT JOIN 
         valorizations ON valorizationitems.valorization_id = valorizations.id
@@ -40,7 +40,6 @@ class Valorization < ActiveRecord::Base
     #ActiveRecord::Base.connection.reconnect!
     #subitem = ActiveRecord::Base.connection.select_all("call test_proc('01%', '2');")
     
-    connection = ActiveRecord::Base.connection
     results = ActiveRecord::Base.connection.execute(str_query) 
 
      
