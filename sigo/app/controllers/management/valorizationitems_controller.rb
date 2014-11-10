3bd395993ba2cb9d63c79cd7730121cd29b936fe
Giancarlo Guzman <giancarloguzo@gmail.com>
2014-11-10 16:31:10 -0500

    metrado acumulado

--- sigo/app/controllers/management/valorizationitems_controller.rb
+++ sigo/app/controllers/management/valorizationitems_controller.rb
@@ -14,16 +14,27 @@ class Management::ValorizationitemsController < ApplicationController
 		if valorization_item != nil 
 			valorization_item.actual_measured = measured_param
 			valorization_item.save
+
+			str_date = valorization.valorization_date.strftime("%Y-%m-%d  %T")
+		    item_amount = ActiveRecord::Base.connection.execute("SELECT get_amount_acumulated('#{item.order}', '#{valorization.budget_id}', '#{str_date}', '#{valorization.id}')").first[0]
+			valorization_item.accumulated_measured = item_amount / item.price
+			valorization_item.save
 		else
 			new_valorization_item = Valorizationitem.new
 			new_valorization_item.valorization_id = valorization.id
 			new_valorization_item.itembybudget_id = item.id
 			new_valorization_item.actual_measured = measured_param
 			new_valorization_item.save
+
+			str_date = valorization.valorization_date.strftime("%Y-%m-%d  %T")
+		    item_amount = ActiveRecord::Base.connection.execute("SELECT get_amount_acumulated('#{item.order}', '#{valorization.budget_id}', '#{str_date}', '#{valorization.id}')").first[0]
+			new_valorization_item.accumulated_measured = item_amount / item.price
+			new_valorization_item.save
 		end
 
-		render nothing: true
+		
 
+		render nothing: true
 	end
 
 end
