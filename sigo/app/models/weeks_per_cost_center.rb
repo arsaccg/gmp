6aa75436f3cc076d3e11a777df96292d8cf16b3a
Eduardo Cabrera <dvex.cabrera@gmail.com>
2014-11-13 15:39:28 -0500

    Planilla.. Modificado para solo 3 conceptos en la tabla

--- sigo/app/models/weeks_per_cost_center.rb
+++ sigo/app/models/weeks_per_cost_center.rb
@@ -1,2 +1,11 @@
 class WeeksPerCostCenter < ActiveRecord::Base
+
+  def self.get_total_hours_per_week cost_center_id, week_id
+    mysql_result = ActiveRecord::Base.connection.execute("SELECT total FROM total_hours_per_week_per_cost_center_" + cost_center_id.to_s + " WHERE status = 1 AND week_id = " + week_id.to_s)
+    if !mysql_result.nil?
+      return mysql_result.first[0]
+    else
+      return 48
+    end
+  end
 end
