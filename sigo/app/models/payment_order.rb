56500313b72b05bd6eaea7681fe843ed1bd0a9a6
Antonny Aguilar <antonny.aguilarc@gmail.com>
2014-11-13 12:38:23 -0500

    ordenes de pago

--- sigo/app/models/payment_order.rb
+++ sigo/app/models/payment_order.rb
@@ -6,25 +6,24 @@ class PaymentOrder < ActiveRecord::Base
     code_category = ActiveRecord::Base.connection.execute("
       SELECT c.code, c.name 
       FROM articles_from_cost_center_" + cost_center_id.to_s + " af, categories c 
-      WHERE af.id = " + article_ids.to_s + " 
+      WHERE af.id IN (" + article_ids.join(',').to_s + ")
       AND af.category_id = c.id 
       GROUP BY af.code"
     ).first[0][0..3]
 
     mysql = ActiveRecord::Base.connection.execute("
-      SELECT name 
+      SELECT name, code
       FROM categories
       WHERE code LIKE '" + code_category.to_s + "'"
-    ).first[0]
+    ).first
 
     if !names_category.include? mysql
-      names_category << mysql
+      names_category << [mysql[0],mysql[1]]
     end
-
     return names_category
   end
 
   def self.get_amount_feo_by_code_phase(code, budget_id)
-    ActiveRecord::Base.connection.execute("select get_amount_feo_by_code('"+code+"', "+budget_id+")").first[0]
+    ActiveRecord::Base.connection.execute("select get_amount_feo_by_code('"+code.to_s+"', "+budget_id.to_s+")").first[0]
   end
 end
