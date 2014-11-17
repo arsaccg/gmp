e5b4eee71d24400de4ec9a8b5d9327cfaec7cf3d
Antonny Aguilar <antonny.aguilarc@gmail.com>
2014-11-17 14:31:50 -0500

    formulas

--- sigo/app/models/payslip.rb
+++ sigo/app/models/payslip.rb
@@ -1,8 +1,12 @@
 class Payslip < ActiveRecord::Base
 
-  def self.generate_payroll_workers cost_center_id, week_id, week_start, week_end
-    result = Array.new
+  def self.generate_payroll_workers cost_center_id, week_id, week_start, week_end, ing, headers
+    @result = Array.new
     total_hour = WeeksPerCostCenter.get_total_hours_per_week(cost_center_id, week_id)
+    @i = 1
+    @result[0] = headers
+    @result[0] << "REMUNERACIÓN BÁSICA"
+    amount = 0
     ActiveRecord::Base.connection.execute("
       SELECT ppd.worker_id, e.dni, CONCAT_WS(' ', e.name, e.second_name, e.paternal_surname, e.maternal_surname), ar.name, pp.date_of_creation, af.type_of_afp, w.numberofchilds, SUM( ppd.normal_hours ) , SUM( 1 ) AS Dias, SUM( ppd.he_60 ) , SUM( ppd.he_100 ) , SUM( ppd.total_hours ) 
       FROM part_people pp, part_person_details ppd, entities e, workers w, worker_afps wa, afps af, worker_contracts wc, articles ar
@@ -17,11 +21,14 @@ class Payslip < ActiveRecord::Base
       AND wc.article_id = ar.id
       GROUP BY ppd.worker_id
     ").each do |row|
+      @result << [ row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10] ]
+
+      amount = 0
       # Remuneracion Básica
       rem_basic = 0
       from_contract = Worker.find(row[0]).worker_contracts.where(:status => 1).first.worker_contract_details.where(:concept_id => 1).first
       if !from_contract.nil?
-        if from_contract.amount != 0
+        if !from_contract.amount.nil?
           rem_basic = (from_contract.amount/8)*row[7]
         else
           article_id = Worker.find(row[0]).worker_contracts.where(:status => 1).first.article_id
@@ -39,45 +46,43 @@ class Payslip < ActiveRecord::Base
           rem_basic = (from_category.amount.to_f/8)*row[7]
         end
       end
+      @result[@i] << rem_basic
 
-      dominicano = rem_basic/6 # Dominical 1/6
-      cts = Concept.find(13).percentage.to_f/100 * rem_basic # % CTS - Data Static
+      ing.each do |ing|
+        con = Concept.find(ing)
+        if con.id != 1
+          if !@result[0].include?(con.name)
+            @result[0] << con.name.to_s
+          end
+          contract = Worker.find(row[0]).worker_contracts.where(:status => 1).first.worker_contract_details.where(:concept_id => ing).first
 
-      # Viaticos
-      viaticos = 0
-      from_contract_viatico = Worker.find(row[0]).worker_contracts.where(:status => 1).first.worker_contract_details.where(:concept_id => 14).first
-      if !from_contract_viatico.nil?
-        if from_contract_viatico.amount != 0
-          viaticos = from_contract_viatico.amount.to_f
-        end
-      else
-        article_id = Worker.find(row[0]).worker_contracts.where(:status => 1).first.article_id
-        category_id = Category.find_by_code(Article.find(article_id).code[2..5]).id
-        from_category_viatico = CategoryOfWorker.find_by_category_id(category_id).category_of_workers_concepts.where(:concept_id => 14).first
-        if from_category_viatico.amount != 0
-          viaticos = from_category_viatico.amount.to_f
-        end
-      end
-
-      # Asignacion Escolar
-      asign_escolar = 0
-      from_contract_asign_escolar = Worker.find(row[0]).worker_contracts.where(:status => 1).first.worker_contract_details.where(:concept_id => 15).first
-      if !from_contract_asign_escolar.nil?
-        if from_contract_asign_escolar.amount != 0
-          asign_escolar = from_contract_asign_escolar.amount.to_f
-        end
-      else
-        article_id = Worker.find(row[0]).worker_contracts.where(:status => 1).first.article_id
-        category_id = Category.find_by_code(Article.find(article_id).code[2..5]).id
-        from_category_asign_escolar = CategoryOfWorker.find_by_category_id(category_id).category_of_workers_concepts.where(:concept_id => 15).first
-        if from_category_asign_escolar.amount != 0
-          asign_escolar = from_category_asign_escolar.amount.to_f
+          if !contract.nil?
+            if contract.amount != 0
+              amount = contract.amount.to_f
+            end
+          else
+            article_id = Worker.find(row[0]).worker_contracts.where(:status => 1).first.article_id
+            category_id = Category.find_by_code(Article.find(article_id).code[2..5]).id
+            from_category = CategoryOfWorker.find_by_category_id(category_id).category_of_workers_concepts.where(:concept_id => ing).first
+            if !from_category.nil?
+              if from_category.amount != 0.0 && !from_category.amount.nil?
+              else
+                formula = con.concept_valorization.formula
+                amount = Formule.translate_formules(formula, rem_basic)
+              end
+            else
+              formula = con.concept_valorization.formula
+              amount = Formule.translate_formules(formula, rem_basic)
+            end
+          end
+          @result[@i] << amount      
         end
       end
 
-      result << [ row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], rem_basic, dominicano, cts, viaticos, asign_escolar ]
+      @i+=1
+      amount = 0
     end
 
-    return result
+    return @result
   end
 end
