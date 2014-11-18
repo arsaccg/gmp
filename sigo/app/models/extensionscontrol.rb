372489569b56ab83998ff03aaa39d2692a81dd32
Giancarlo Guzman <giancarloguzo@gmail.com>
2014-11-18 15:11:31 -0500

    edit ampliacion + indice wbsitems mal cod

--- sigo/app/models/extensionscontrol.rb
+++ sigo/app/models/extensionscontrol.rb
@@ -1,6 +1,12 @@
 class Extensionscontrol < ActiveRecord::Base
 	belongs_to :project
 	has_attached_file :files
+
+	STATUS_HASH = {
+		"requested" => "Requerido",
+		"approved" => "Aprobado",
+		"disproved" => "Desaprobado"
+	}
 	
 	state_machine :status, :initial => :requested do
 		event :approve do
