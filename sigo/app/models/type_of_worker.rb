81701f8e07ab58fb2fbd5595f04255c516ac1767
Antonny Aguilar <antonny.aguilarc@gmail.com>
2014-12-26 10:02:08 -0500

    fixes planillas

--- sigo/app/models/type_of_worker.rb
+++ sigo/app/models/type_of_worker.rb
@@ -1,4 +1,7 @@
 class TypeOfWorker < ActiveRecord::Base
   has_many :workers
   has_many :type_of_payslip
+
+  include ActiveModel::Validations
+  validates :name, :uniqueness => { :message => "El nombre debe ser unico."}, :allow_blank => true, :case_sensitive => false
 end
