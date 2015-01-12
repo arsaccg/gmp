3b76c21fcf25cb4bf3234e79bc16d96167dfc5bf
Eduardo Cabrera <dvex.cabrera@gmail.com>
2015-01-12 12:09:47 -0500

    Conexion cambiada a una nueva url de webservice

--- tobi/lib/sqlserver/dbconnector.rb
+++ tobi/lib/sqlserver/dbconnector.rb
@@ -16,11 +16,18 @@ module DBConnector
 		puts query
 
 		query_str = server.to_s + ":" + db_name.to_s + ":" + user.to_s + ":" + pass.to_s + ":" + query.to_s
-
+		/
 		client = Savon.client(wsdl: @@url_webservice)
-		puts client
+		puts client.inspect
 		response = client.call(:do_query, message: { query: query_str })
 		result = response.body[:do_query_response][:do_query_result]
+		/
+		uri = URI('http://10.10.10.220/webservice')
+	    params = { :str => query_str }
+	    uri.query = URI.encode_www_form(params)
+
+	    result = Net::HTTP.get_response(uri)
+
 		json=JSON.parse(result)
 		return json
 	end
@@ -31,7 +38,7 @@ module DBConnector
 
 		#@@url_webservice = "http://192.168.1.123/Extractor.asmx?wsdl"
 		
-		@@url_webservice = params[:url] == nil ? "http://10.10.10.20/Extractor.asmx?wsdl" : "http://#{params[:url]}/Extractor.asmx?wsdl"
+		@@url_webservice = params[:url] == nil ? "http://10.10.10.220/webservice?str" : "http://#{params[:url]}/webservice?str"
 		@@server = params[:server] == nil ? "SRVBDS10\\S10" : params[:server]
 		@@db_name = params[:db_name] == nil ? "master" : params[:db_name]
 		@@user = params[:user] == nil ? "sa" : params[:user]
