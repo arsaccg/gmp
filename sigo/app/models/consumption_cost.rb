5e7eab6a807f9d4325dfbe4e9eb75c8e6379ac38
Antonny Aguilar <antonny.aguilarc@gmail.com>
2015-03-13 12:30:55 -0500

    tabla

--- sigo/app/models/consumption_cost.rb
+++ sigo/app/models/consumption_cost.rb
@@ -2,15 +2,14 @@ class ConsumptionCost < ActiveRecord::Base
   establish_connection :external
 
   def self.apply_all_consult
-    connection.select_one("SELECT 0, `general_exp_mo_valoriz`, 0, `general_exp_mo_costreal`, `general_exp_mo_meta` FROM `actual_consumption_cost_actual_january`")
-
-    connection.select_one("SELECT 0, `general_exp_mat_valoriz`, 0, `general_exp_mat_costreal`,`general_exp_mat_meta` FROM `actual_consumption_cost_actual_january`")
-
-    connection.select_one("SELECT 0, `general_exp_subcont_valoriz`, 0, `general_exp_subcont_costreal`, `general_exp_subcont_meta` FROM `actual_consumption_cost_actual_january`")
-
-    connection.select_one("SELECT 0, `general_exp_serv_valoriz`, 0, `general_exp_serv_costreal`, `general_exp_serv_meta` FROM `actual_consumption_cost_actual_january`")
-
-    connection.select_one("SELECT 0, `general_exp_equip_valoriz`, 0, `general_exp_equip_costreal`, `general_exp_equip_meta` FROM `actual_consumption_cost_actual_january`")
-
+    array_ge = connection.select_one("
+      SELECT 
+      0, `general_exp_mo_valoriz`, 0, `general_exp_mo_costreal`, `general_exp_mo_meta` ,
+      0, `general_exp_mat_valoriz`, 0, `general_exp_mat_costreal`,`general_exp_mat_meta` ,
+      0, `general_exp_subcont_valoriz`, 0, `general_exp_subcont_costreal`, `general_exp_subcont_meta` ,
+      0, `general_exp_serv_valoriz`, 0, `general_exp_serv_costreal`, `general_exp_serv_meta` ,
+      0, `general_exp_equip_valoriz`, 0, `general_exp_equip_costreal`, `general_exp_equip_meta` 
+    FROM `actual_consumption_cost_actual_january`")
+    return array_ge
   end
 end
