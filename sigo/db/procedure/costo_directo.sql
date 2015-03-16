b88249e969a249caef08274e9ed863211c70f1dd
Eduardo Cabrera <dvex.cabrera@gmail.com>
2015-03-16 12:19:54 -0500

    Valorizado, montos creibles!

--- sigo/db/procedure/costo_directo.sql
+++ sigo/db/procedure/costo_directo.sql
@@ -1,8 +1,11 @@
+use local_system_sigo;
+
 DELIMITER $$
 DROP PROCEDURE IF EXISTS `costo_directo_valorizado_por_mes`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `costo_directo_valorizado_por_mes`()
 BEGIN
   DECLARE done INT DEFAULT FALSE;
+  DECLARE ibi_done INT DEFAULT FALSE;
 
   DECLARE real_cost_hand_work FLOAT(10,2);
   DECLARE real_cost_materials FLOAT(10,2);
@@ -32,34 +35,37 @@ BEGIN
       LEAVE read_loop;
     END IF;
 
-	-- OTRO MONTO
+	-- MONTOS VALORIZADOS
 	BLOCK: BEGIN
 
 	  DECLARE itembybudgets CURSOR FOR
 		SELECT rv.order, description, con_measured FROM report_valorizations rv WHERE valorization_id = v_valorization_id AND con_measured IS NOT NULL;
-	  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+	  DECLARE CONTINUE HANDLER FOR NOT FOUND SET ibi_done = TRUE;
 	
 	  OPEN itembybudgets;
 	  read_loop_ibb: LOOP
 		FETCH itembybudgets INTO v_order, v_description, v_measured;
+		IF ibi_done THEN
+		  LEAVE read_loop_ibb;
+		END IF;
 
   		SET @i_identify = (SELECT p.id FROM itembywbses ibw, wbsitems wi, phases p WHERE ibw.wbsitem_id = wi.id AND p.code > '0___' AND p.code < '89__' AND wi.phase_id = p.id AND ibw.budget_id = v_budget_id AND ibw.order_budget LIKE v_order GROUP BY p.id);
 
 		IF @i_identify IS NOT NULL THEN
 			SET @real_cost_hand_work = (SELECT SUM( ibi.quantity * ibi.price * ib.measured ) FROM inputbybudgetanditems AS ibi, itembybudgets AS ib WHERE ibi.cod_input LIKE  '01%' AND ibi.budget_id = v_budget_id AND ib.budget_id = v_budget_id AND ibi.`order` LIKE CONCAT(v_order, '%') AND ibi.`order` LIKE CONCAT( ib.`order` ,  '%' ));
-			SET real_cost_hand_work  = real_cost_hand_work + IFNULL(@real_cost_hand_work, 0);
+			SET real_cost_hand_work  = IFNULL(real_cost_hand_work, 0) + IFNULL(@real_cost_hand_work, 0);
 
 			SET @real_cost_materials = (SELECT SUM( ibi.quantity * ibi.price * ib.measured ) FROM inputbybudgetanditems AS ibi, itembybudgets AS ib WHERE ibi.cod_input LIKE  '02%' AND ibi.budget_id = v_budget_id AND ib.budget_id = v_budget_id AND ibi.`order` LIKE CONCAT(v_order, '%') AND ibi.`order` LIKE CONCAT( ib.`order` ,  '%' ));
-			SET real_cost_materials  = real_cost_materials + IFNULL(@real_cost_materials, 0);
+			SET real_cost_materials  = IFNULL(real_cost_materials, 0) + IFNULL(@real_cost_materials, 0);
 
 			SET @real_cost_equipment = (SELECT SUM( ibi.quantity * ibi.price * ib.measured ) FROM inputbybudgetanditems AS ibi, itembybudgets AS ib WHERE ibi.cod_input LIKE  '03%' AND ibi.budget_id = v_budget_id AND ib.budget_id = v_budget_id AND ibi.`order` LIKE CONCAT(v_order, '%') AND ibi.`order` LIKE CONCAT( ib.`order` ,  '%' ));
-			SET real_cost_equipment  = real_cost_equipment + IFNULL(@real_cost_equipment, 0);
+			SET real_cost_equipment  = IFNULL(real_cost_equipment, 0) + IFNULL(@real_cost_equipment, 0);
 
 			SET @real_cost_subcontract = (SELECT SUM( ibi.quantity * ibi.price * ib.measured ) FROM inputbybudgetanditems AS ibi, itembybudgets AS ib WHERE ibi.cod_input LIKE  '04%' AND ibi.budget_id = v_budget_id AND ib.budget_id = v_budget_id AND ibi.`order` LIKE CONCAT(v_order, '%') AND ibi.`order` LIKE CONCAT( ib.`order` ,  '%' ));
-			SET real_cost_subcontract  = real_cost_subcontract + IFNULL(@real_cost_subcontract, 0);
+			SET real_cost_subcontract  = IFNULL(real_cost_subcontract, 0) + IFNULL(@real_cost_subcontract, 0);
 
 			SET @real_cost_service = (SELECT SUM( ibi.quantity * ibi.price * ib.measured ) FROM inputbybudgetanditems AS ibi, itembybudgets AS ib WHERE ibi.cod_input LIKE  '05%' AND ibi.budget_id = v_budget_id AND ib.budget_id = v_budget_id AND ibi.`order` LIKE CONCAT(v_order, '%') AND ibi.`order` LIKE CONCAT( ib.`order` ,  '%' ));
-			SET real_cost_service  = real_cost_service + IFNULL(@real_cost_service, 0);
+			SET real_cost_service  = IFNULL(real_cost_service, 0) + IFNULL(@real_cost_service, 0);
 
 		END IF;
 
