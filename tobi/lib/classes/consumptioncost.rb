a295298b6bc1d743dc04ebacd5b8ecd03a45acbc
Antonny Aguilar <antonny.aguilarc@gmail.com>
2015-04-22 13:13:06 -0500

    lib

--- tobi/lib/classes/consumptioncost.rb
+++ tobi/lib/classes/consumptioncost.rb
@@ -1,27 +1,43 @@
 class ConsumptionCostObj
 
-  attr_accessor :name, :programado, :valorizado, :valorGanado, :real, :meta
+  attr_accessor :name, :programado, :valorizado, :valorGanado, :real, :meta, :type
 
-  def initialize(metadata, type_amount)
+  def initialize(metadata, type_amount,type)
     @name = metadata['str_article']
-
+    @type = type
     if type_amount.include?('specific')
-      @programado = (metadata['programado_' + type_amount] rescue '-')
-      @valorizado = (metadata['valorizado_' + type_amount] rescue '-')
-      @valorGanado = (metadata['valor_ganado_' + type_amount] rescue '-')
-      @real = (metadata['real_' + type_amount] rescue '-')
-      @meta = (metadata['meta_' + type_amount] rescue '-')
+      @programado = (metadata['programado_' + type_amount])
+      @valorizado = (metadata['valorizado_' + type_amount])
+      @valorGanado = (metadata['valor_ganado_' + type_amount])
+      @real = (metadata['real_' + type_amount])
+      @meta = (metadata['meta_' + type_amount])
     elsif type_amount.include?('measured')
-      @programado = (metadata[type_amount + '_programado'] rescue '-')
-      @valorizado = (metadata[type_amount + '_valorizado'] rescue '-')
-      @valorGanado = (metadata[type_amount + '_valor_ganado'] rescue '-')
-      @real = (metadata[type_amount + '_real'] rescue '-')
-      @meta = (metadata[type_amount + '_meta'] rescue '-')
+      @programado = (metadata[type_amount + '_programado'])
+      @valorizado = (metadata[type_amount + '_valorizado'])
+      @valorGanado = (metadata[type_amount + '_valor_ganado'])
+      @real = (metadata[type_amount + '_real'])
+      @meta = (metadata[type_amount + '_meta'])
     else
       @programado = '-'
       @valorizado = '-'
       @valorGanado = '-'
       @real = '-'
+      @meta = '-'
+    end
+
+    if @programado.to_f == 0.0
+      @programado = '-'
+    end
+    if @valorizado.to_f == 0.0
+      @valorizado = '-'
+    end
+    if @valorGanado.to_f == 0.0
+      @valorGanado = '-'
+    end
+    if @real.to_f == 0.0
+      @real = '-'
+    end
+    if @meta.to_f == 0.0
       @meta = '-'
     end
 
@@ -33,5 +49,6 @@ class ConsumptionCostObj
   def to_s; valorGanado.to_s; end
   def to_s; real.to_s; end
   def to_s; meta.to_s; end
+  def to_s; type.to_s; end
 
 end
--- tobi/lib/classes/consumptioncosttotal.rb
+++ tobi/lib/classes/consumptioncosttotal.rb
@@ -1,8 +1,8 @@
 class ConsumptionCostTotalObj
 
-  attr_accessor :name, :programado, :valorizado, :valorGanado, :real, :meta
+  attr_accessor :name, :programado, :valorizado, :valorGanado, :real, :meta, :type
 
-  def initialize(metadata, metadata_sum, type_amount)
+  def initialize(metadata, metadata_sum, type_amount, type)
     if !metadata['str_fase_padre'].nil?
       @name = metadata['str_fase_padre']
     elsif !metadata['str_fase_hijo'].nil?
@@ -18,19 +18,19 @@ class ConsumptionCostTotalObj
     else
       @name = "-"
     end
-
+    @type = type
     if type_amount.include?('specific')
-      @programado = (metadata_sum['programado_' + type_amount] rescue '-')
-      @valorizado = (metadata_sum['valorizado_' + type_amount] rescue '-')
-      @valorGanado = (metadata_sum['valor_ganado_' + type_amount] rescue '-')
-      @real = (metadata_sum['real_' + type_amount] rescue '-')
-      @meta = (metadata_sum['meta_' + type_amount] rescue '-')
+      @programado = (metadata_sum['programado_' + type_amount])
+      @valorizado = (metadata_sum['valorizado_' + type_amount])
+      @valorGanado = (metadata_sum['valor_ganado_' + type_amount])
+      @real = (metadata_sum['real_' + type_amount])
+      @meta = (metadata_sum['meta_' + type_amount])
     elsif type_amount.include?('measured')
-      @programado = (metadata_sum[type_amount + '_programado'] rescue '-')
-      @valorizado = (metadata_sum[type_amount + '_valorizado'] rescue '-')
-      @valorGanado = (metadata_sum[type_amount + '_valor_ganado'] rescue '-')
-      @real = (metadata_sum[type_amount + '_real'] rescue '-')
-      @meta = (metadata_sum[type_amount + '_meta'] rescue '-')
+      @programado = (metadata_sum[type_amount + '_programado'])
+      @valorizado = (metadata_sum[type_amount + '_valorizado'])
+      @valorGanado = (metadata_sum[type_amount + '_valor_ganado'])
+      @real = (metadata_sum[type_amount + '_real'])
+      @meta = (metadata_sum[type_amount + '_meta'])
     else
       @programado = '-'
       @valorizado = '-'
@@ -39,6 +39,21 @@ class ConsumptionCostTotalObj
       @meta = '-'
     end
 
+    if @programado.to_f == 0.0
+      @programado = '-'
+    end
+    if @valorizado.to_f == 0.0
+      @valorizado = '-'
+    end
+    if @valorGanado.to_f == 0.0
+      @valorGanado = '-'
+    end
+    if @real.to_f == 0.0
+      @real = '-'
+    end
+    if @meta.to_f == 0.0
+      @meta = '-'
+    end    
   end
 
   def to_s; name.to_s; end
@@ -47,5 +62,6 @@ class ConsumptionCostTotalObj
   def to_s; valorGanado.to_s; end
   def to_s; real.to_s; end
   def to_s; meta.to_s; end
+  def to_s; type.to_s; end
 
 end
